ddf2ca4218fe3fd46b257e70789299d64d41b226
Mark Wales <mark@smallhadroncollider.com>
2015-11-24 17:05:07 +0000

    added app template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en-GB">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    </head>
+    <body class="container">
+        @yield("content")
+    </body>
+</html>
